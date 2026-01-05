42dd9f9741641a0ddb12d3eed4b760132f10978e
Matt Stoker <matt.stoker@gmail.com>
2026-01-04 20:37:53 -0700

    Added execution behavior to macOS GUI

--- GUI/SilkMac/App.swift
+++ GUI/SilkMac/App.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import SilkCPU
 
+// MARK: - App
+
 @main
 struct SilkMacApp: App {
     var body: some Scene {
@@ -17,58 +19,15 @@ struct SilkMacApp: App {
     }
 }
 
-class System: ObservableObject {
-    @Published var cpu: CPU6502 = CPU6502(load: { _ in 0 }, store: { _, _ in })
-    @Published var memory: [UInt8]
-    init(cpu: CPU6502 = CPU6502(load: { _ in 0 }, store: { _, _ in }), memory: [UInt8] = Array((0x0000...0xFFFF).map { _ in UInt8.random(in: 0x00...0xFF) })) {
-        self.memory = memory
-        self.cpu = CPU6502(
-            pc: cpu.pc,
-            ac: cpu.ac,
-            xr: cpu.xr,
-            yr: cpu.yr,
-            sr: cpu.sr,
-            sp: cpu.sp,
-            state: cpu.state,
-            load: { [weak self] address in return self?.memory[Int(address)] ?? 0xEA },
-            store: { [weak self] address, value in self?.memory[Int(address)] = value }
-        )
-    }
-    
-    func screenshot() -> NSImage {
-        let ppm = Self.screenshot(cpu: cpu, start: 0x2000, end: 0x4000, width: 0x7F)
-        let image = NSImage(data: ppm.data(using: .utf8)!)!
-        return image
-    }
-    
-    static func screenshot(cpu: CPU6502, start: UInt16, end: UInt16, width: Int) -> String {
-        let count = Int(end) - Int(start) + 1
-        let height = count / width
-        var screenshot = ""
-        screenshot.append("P3\n")
-        screenshot.append("\(width) \(height)\n")
-        screenshot.append("3\n")
-        for y in 0..<height {
-            for x in 0..<width {
-                let pixelIndex = y * width + x
-                let address = pixelIndex > count ? nil : start + UInt16(pixelIndex)
-                let value: UInt8 = address.map { cpu.load($0) } ?? UInt8.min
-                let r = ((value & 0b00000011) >> 0)
-                let g = ((value & 0b00001100) >> 2)
-                let b = ((value & 0b00110000) >> 4)
-                screenshot.append("\(r) \(g) \(b)\n")
-            }
-        }
-        return screenshot
-    }
-}
+// MARK: - UI
 
 struct ContentView: View {
     @State var programImporterShowing = false
     @State var programFile: URL?
     @State var programOffset: Int = 0xE000 // TODO: UI
     @State var showMemory: Bool = false
-    @State var showVideo: Bool = false
+    @State var showVideo: Bool = true
+    @State var stepCount: Int = 1
     @State var stepTimer: Timer? = nil
     @State var log: String = ""
     @EnvironmentObject var system: System
@@ -84,6 +43,8 @@ struct ContentView: View {
             TextEditor(text: $log)
                 .disabled(true)
             VStack {
+                // MARK: Program
+                
                 Button(
                     action: { programImporterShowing = true },
                     label: { Text("Load Program") }
@@ -101,37 +62,45 @@ struct ContentView: View {
                         var program: [UInt8] = Array(repeating: 0x00, count: programData.count)
                         programData.copyBytes(to: &program, count: min(program.count, programData.count))
                         
-                        for index in program.indices {
-                            system.memory[programOffset + index] = program[index]
-                        }
+                        system.load(data: program, startingAt: UInt16(programOffset))
                     }
                 }
-                if stepTimer == nil {
-                    Button(
-                        action: {
-                            system.cpu.execute()
-                            log += "\(system.cpu.debugDescription)\n"
-                        },
-                        label: { Text("Step") }
-                    )
-                    Button(
-                        action: {
-                            log = ""
-                            stepTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
-                                system.cpu.execute()
-                            }
-                        },
-                        label: { Text("Run") }
-                    )
-                } else {
-                    Button(
-                        action: {
-                            stepTimer?.invalidate()
-                            stepTimer = nil
-                        },
-                        label: { Text("Pause") }
-                    )
+                
+                // MARK: Execution
+                
+                if system.cpu.state == .run {
+                    if stepTimer == nil {
+                        Button(
+                            action: {
+                                system.execute(count: stepCount)
+                                log += "\(system.cpu.debugDescription)\n"
+                            },
+                            label: { Text("Step \(stepCount)") }
+                        )
+                        TextField("Count", value: $stepCount, formatter: NumberFormatter())
+                            .frame(width: 50)
+                        Button(
+                            action: {
+                                log = ""
+                                stepTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { _ in
+                                    system.execute()
+                                }
+                            },
+                            label: { Text("Run") }
+                        )
+                    } else {
+                        Button(
+                            action: {
+                                stepTimer?.invalidate()
+                                stepTimer = nil
+                            },
+                            label: { Text("Pause") }
+                        )
+                    }
                 }
+                
+                // MARK: CPU State
+                
                 VStack {
                     HStack {
                         Text("PC")
@@ -162,6 +131,9 @@ struct ContentView: View {
                         Text(String(describing: system.cpu.state))
                     }
                 }
+                
+                // MARK: Memory State
+                
                 Toggle("Memory View", isOn: $showMemory)
                 if showMemory {
                     Table(system.memory.indices.map { MemoryEntry(address: UInt16($0), value: system.memory[$0]) }) {
@@ -183,5 +155,72 @@ struct ContentView: View {
             }
             .padding()
         }
+    }
+}
+
+// MARK: - System
+
+class System: ObservableObject {
+    @Published var cpu: CPU6502 = CPU6502(load: { _ in 0 }, store: { _, _ in })
+    @Published var memory: [UInt8] = Array((0x0000...0xFFFF).map { _ in UInt8.min })
+    init() {
+        reset()
+    }
+    
+    func reset() {
+        self.memory = Array((0x0000...0xFFFF).map { _ in UInt8.random(in: 0x00...0xFF) })
+        self.cpu = CPU6502(
+            pc: cpu.pc,
+            ac: cpu.ac,
+            xr: cpu.xr,
+            yr: cpu.yr,
+            sr: cpu.sr,
+            sp: cpu.sp,
+            state: cpu.state,
+            load: { [weak self] address in return self?.memory[Int(address)] ?? 0xEA },
+            store: { [weak self] address, value in self?.memory[Int(address)] = value }
+        )
+    }
+    
+    func load(data: [UInt8], startingAt offset: UInt16) {
+        reset()
+        // TODO: What if data is too large or offset causes overlap?
+        for index in data.indices {
+            memory[Int(offset + UInt16(index))] = data[index]
+        }
+        cpu.execute()
+    }
+    
+    func execute(count: Int = 1) {
+        for _ in 0..<count {
+            cpu.execute()
+        }
+    }
+    
+    func screenshot() -> NSImage {
+        let ppm = Self.screenshot(cpu: cpu, start: 0x2000, end: 0x4000, width: 0x80)
+        let image = NSImage(data: ppm.data(using: .utf8)!)!
+        return image
+    }
+    
+    static func screenshot(cpu: CPU6502, start: UInt16, end: UInt16, width: Int) -> String {
+        let count = Int(end) - Int(start) + 1
+        let height = count / width
+        var screenshot = ""
+        screenshot.append("P3\n")
+        screenshot.append("\(width) \(height)\n")
+        screenshot.append("3\n")
+        for y in 0..<height {
+            for x in 0..<width {
+                let pixelIndex = y * width + x
+                let address = pixelIndex > count ? nil : start + UInt16(pixelIndex)
+                let value: UInt8 = address.map { cpu.load($0) } ?? UInt8.min
+                let r = ((value & 0b00000011) >> 0)
+                let g = ((value & 0b00001100) >> 2)
+                let b = ((value & 0b00110000) >> 4)
+                screenshot.append("\(r) \(g) \(b)\n")
+            }
+        }
+        return screenshot
     }
 }
