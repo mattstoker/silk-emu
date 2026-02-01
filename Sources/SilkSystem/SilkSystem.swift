d4386485c3d8ac224a1aeb9a2e34094343450d79
Matt Stoker <matt.stoker@gmail.com>
2026-01-31 20:48:13 -0700

    Fixed ambiguous call signature

--- Sources/SilkSystem/SilkSystem.swift
+++ Sources/SilkSystem/SilkSystem.swift
@@ -126,7 +126,7 @@ public class System {
         rom.program(data: data, startingAt: offset)
     }
     
-    public func execute(count: Int = 1) -> (instruction: CPU6502.Instruction, oper: UInt8?, operWideHigh: UInt8?) {
+    public func execute() -> (instruction: CPU6502.Instruction, oper: UInt8?, operWideHigh: UInt8?) {
         return cpu.execute()
     }
     
